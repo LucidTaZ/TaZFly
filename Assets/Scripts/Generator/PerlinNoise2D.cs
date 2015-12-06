bb4df576d4938135132bb4e8f9144e7898945de9
Thijs Zumbrink <tazaranie@gmail.com>
2015-12-06 19:35:35 +0100

    Tweak level generation
    Adjust the terrain so that lengthwise trenches appear that reward the
    player significantly by allowing him to stay low longer.

--- Assets/Scripts/Generator/PerlinNoise2D.cs
+++ Assets/Scripts/Generator/PerlinNoise2D.cs
@@ -2,9 +2,9 @@
 
 public class PerlinNoise2D : MonoBehaviour, INoise2D
 {
-
-	public int FrequencyScale = 15;
+	public int FrequencyScale = 15; // Higher = smoother, bigger terrain
 	public float BaselineImportance = 0.6f;
+	public float Skew = 1.0f; // Over 1 = stretch lengthwise, under 1 = stretch broadwise. Used to give the player some advantage of flying into a good spot, by allowing him to stay there longer
 
 	/**
 	 * Generate heightmap pixel
@@ -16,7 +16,7 @@ public class PerlinNoise2D : MonoBehaviour, INoise2D
 	public float Sample(Vector2 point)
 	{
 		float baselineHeight = Mathf.PerlinNoise(point.y / FrequencyScale / 5 + 9999, 0f);
-		float detailHeight = Mathf.PerlinNoise(point.x / FrequencyScale, point.y / FrequencyScale);
+		float detailHeight = Mathf.PerlinNoise(point.x * Skew / FrequencyScale, point.y / Skew / FrequencyScale);
 		return Mathf.Lerp(detailHeight, baselineHeight, BaselineImportance);
 	}
 
