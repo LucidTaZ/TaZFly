2ef3692666e3b597fdcfc21b5240de370b2cd513
Thijs Zumbrink <tazaranie@gmail.com>
2015-12-06 19:42:27 +0100

    Add text field with number of enemies

--- Assets/Scripts/GUIController.cs
+++ Assets/Scripts/GUIController.cs
@@ -6,6 +6,7 @@
 public class GUIController : MonoBehaviour {
 	public GUIText HitpointsText;
 	public GUIText SpeedText;
+	public GUIText EnemiesText;
 
 	Hitpoints hitpoints;
 	Rigidbody subject;
@@ -16,9 +17,14 @@ public class GUIController : MonoBehaviour {
 	}
 
 	void Update () {
-		if (subject != null) { // Happens during level reload
+		if (canUpdate()) {
 			HitpointsText.text = System.String.Format("Hitpoints: {0:P0}", Mathf.Max(0, hitpoints.GetRelativeHitpoints()));
 			SpeedText.text = System.String.Format("Speed: {0:N2}", subject.velocity.z);
+			EnemiesText.text = System.String.Format("Enemies: {0:N0}", GameObject.FindGameObjectsWithTag("Ship").Length - 1); // This can probably be more efficient. Maybe via a listener approach?
 		}
+	}
+
+	bool canUpdate() {
+		return subject != null; // Happens during level reload
 	}
 }
