a7ff6289b28471b94a54871cec24bd9acfc86c6b
Thijs Zumbrink <tazaranie@gmail.com>
2015-12-06 17:52:48 +0100

    Add propeller audio

---
+++ Assets/Scripts/AudioPitchBySpeed.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AudioPitchBySpeed : MonoBehaviour {
+
+	public float ScaleFactor = 0.03f; // Factor to convert speed into pitch increment
+	public float NeutralSpeed = 5f;
+	float startPitch;
+
+	void Start () {
+		startPitch = GetComponent<AudioSource>().pitch;
+	}
+	
+	void Update () {
+		//float speed = GetComponent<Rigidbody>().velocity.magnitude; // Could use this but I think it would be higher during steering, which would feel unnatural
+		float speed = GetComponent<Rigidbody>().velocity.z;
+		GetComponent<AudioSource>().pitch = startPitch + ((speed - NeutralSpeed) * ScaleFactor);
+	}
+}
---
+++ Assets/Scripts/AudioVolumeByPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/**
+ * Change audio volume based on whether the source is the player
+ *
+ * We simply look at whether we have a PlayerController. That might need to be changed at some point, but for now it
+ * works.
+ */
+public class AudioVolumeByPlayer : MonoBehaviour {
+
+	public float PlayerVolume = 1.0f;
+	public float NonPlayerVolume = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		if (GetComponent<PlayerController>()) {
+			GetComponent<AudioSource>().volume = PlayerVolume;
+		} else {
+			GetComponent<AudioSource>().volume = NonPlayerVolume;
+		}
+	}
+}
