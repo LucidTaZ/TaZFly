f4cb1074bc106aea54e7c9956ee5ae422d3dd6d4
Thijs Zumbrink <tazaranie@gmail.com>
2016-01-28 21:25:12 +0100

    Add CameraSwoon to make the camera move in the player's direction

---
+++ Assets/Scripts/CameraSwoon.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CameraSwoon : MonoBehaviour {
+
+	public float MaxDeviation = 0.2f;
+	public float ConvergeSpeed = 0.6f;
+
+	private GameObject Subject;
+	private bool loaded = false;
+
+	private Quaternion rotation;
+
+	public void Load (GameObject subject) {
+		Subject = subject;
+		loaded = true;
+	}
+
+	public void Unload () {
+		loaded = false;
+	}
+
+	void Start () {
+		// Take over the relative settings modeled in the editor.
+		rotation = transform.rotation;
+	}
+	
+	void LateUpdate () {
+		if (loaded) {
+			Quaternion maximumDeviation = Quaternion.Slerp(rotation, Subject.transform.rotation, MaxDeviation);
+			transform.rotation = Quaternion.Slerp(transform.rotation, maximumDeviation, ConvergeSpeed * Time.deltaTime);
+		}
+	}
+}
