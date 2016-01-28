0108e42fef8178dd0e179ada50784b0f756c38d5
Thijs Zumbrink <tazaranie@gmail.com>
2016-01-28 22:27:07 +0100

    Add camera shake

--- Assets/Scripts/CollisionExplosion.cs
+++ Assets/Scripts/CollisionExplosion.cs
@@ -13,6 +13,11 @@ public class CollisionExplosion : MonoBehaviour {
 			gameObject.GetComponent<AudioSource>().Play();
 			soundPlayed = true;
 			DestroyObject(gameObject, gameObject.GetComponent<AudioSource>().clip.length); // Destroy when the sound clip ends
+
+			CameraShake shake;
+			if ((shake = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraShake>()) != null) {
+				shake.ShakeAtLocation(transform.position);
+			}
 		}
 		gameObject.GetComponent<MeshRenderer>().enabled = false;
 		Collider[] colliders = gameObject.GetComponents<Collider>();
