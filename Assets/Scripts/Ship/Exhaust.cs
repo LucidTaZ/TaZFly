5edeb71a148309ad551b454d67fc0f3a3c868aba
Thijs Zumbrink <tazaranie@gmail.com>
2017-09-13 21:33:53 +0200

    Adjust ship exhaust to use trail renderer
    This is done on Unity 2017.1.1f1 so it may not immediately be mergable.

---
+++ Assets/Scripts/Ship/Exhaust.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TrailRenderer))]
+public class Exhaust : MonoBehaviour {
+	TrailRenderer trailRenderer;
+	float originalTrailTime;
+
+	void Awake () {
+		trailRenderer = GetComponent<TrailRenderer>();
+		originalTrailTime = trailRenderer.time;
+		trailRenderer.time = 0.0f;
+	}
+
+	// Severity from 0 to 1
+	public void SetSeverity (float severity) {
+		trailRenderer.widthMultiplier = severity;
+		trailRenderer.time = severity * originalTrailTime;
+	}
+}
